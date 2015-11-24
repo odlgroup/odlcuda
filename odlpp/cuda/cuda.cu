b682df5aa60a92c5be7b24595c03f65f45951fbd
Jonas Adler <jonasadl@kth.se>
2015-11-24 17:40:15 +0100

    BUG: fixed a bug in gaussian filter

--- odlpp/cuda/cuda.cu
+++ odlpp/cuda/cuda.cu
@@ -184,8 +184,8 @@ void forwardDifference2DAdjointImpl(const DeviceVector<float>& dx,
 __global__ void gaussianBlurXkernel(const float* source, float* target,
                                     const uint2 imageSize, float sigma_x,
                                     int kernel_width) {
-    uint2 id = make_uint2(blockIdx.x * blockDim.x + threadIdx.x,
-                          blockIdx.y * blockDim.y + threadIdx.y);
+    int2 id = make_int2(blockIdx.x * blockDim.x + threadIdx.x,
+                        blockIdx.y * blockDim.y + threadIdx.y);
 
     if (id.x >= imageSize.x || id.y >= imageSize.y) return;
 
@@ -212,8 +212,8 @@ __global__ void gaussianBlurXkernel(const float* source, float* target,
 __global__ void gaussianBlurYkernel(const float* source, float* target,
                                     const uint2 imageSize, float sigma_y,
                                     int kernel_height) {
-    uint2 id = make_uint2(blockIdx.x * blockDim.x + threadIdx.x,
-                          blockIdx.y * blockDim.y + threadIdx.y);
+    int2 id = make_int2(blockIdx.x * blockDim.x + threadIdx.x,
+                        blockIdx.y * blockDim.y + threadIdx.y);
 
     if (id.x >= imageSize.x || id.y >= imageSize.y) return;
 
@@ -248,14 +248,14 @@ void gaussianBlurImpl(const DeviceVector<float>& image,
 
     gaussianBlurXkernel<<<dimGrid, dimBlock>>>(image.data(),
                                                temporary.data(),
-                                               make_uint2( narrow_cast<unsigned>(image_width),
-                                                     narrow_cast<unsigned>(image_height)),
+                                               make_uint2(narrow_cast<unsigned>(image_width),
+                                                          narrow_cast<unsigned>(image_height)),
                                                sigma_x, kernel_width);
     CUDA_KERNEL_ERRCHECK;
     gaussianBlurYkernel<<<dimGrid, dimBlock>>>(temporary.data(),
                                                out.data(),
-                                               make_uint2( narrow_cast<unsigned>(image_width),
-                                                     narrow_cast<unsigned>(image_height)),
+                                               make_uint2(narrow_cast<unsigned>(image_width),
+                                                          narrow_cast<unsigned>(image_height)),
                                                sigma_y,
                                                kernel_height);
     CUDA_KERNEL_ERRCHECK;
