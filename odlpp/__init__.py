79022135f6a11557c6bead6d4fff4f460fdd6373
Holger Kohr <kohr@kth.se>
2015-12-08 17:39:43 +0100

    ENH: make build and install work with Python 3
    Cmake now uses the `PythonInterp` finder and applies `PYTHON_EXECUTABLE`
    when running `setup.py install`. This automatically installs into the
    right location instead of the hard-coded one.
    The relative import in `__init__.py` is fixed, now the package works
    with Python 3.

--- odlpp/__init__.py
+++ odlpp/__init__.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
-import odlpp_cuda
+from __future__ import absolute_import
+
+from . import odlpp_cuda
 
 # __all__ = ['']
